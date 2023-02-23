f7f8e309f26d22e2cf64f8b4edd7b022ed7498bd
Splenz14 <mhizjennysplenz@gmail.com>
2023-02-23 15:01:34 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+
+if (c .= 'A' 77 c <= 'Z')
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
